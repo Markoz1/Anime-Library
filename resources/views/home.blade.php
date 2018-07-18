d5ff147b1dd4dbfc17b61d43f31a9bba9c902d1a
markoz1 <marco.jhoel@gmail.com>
2018-07-18 08:39:51 -0400

    modular-admin config, layout main

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+@section('content')
+<article class="content d-flex align-items-center justify-content-center">
+    <section class="section">
+        <h1 class="display-1 text-center">Bienvenido!</h1>
+        <h1 class="display-3 text-center"></h1>
+    </section>
+</article>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        {{-- ModularAdmin css --}}
+        <link rel="stylesheet" href="{{ asset('modular-admin/css/vendor.css') }}">
+        {{-- Theme --}}
+        <link rel="stylesheet" href="{{ asset('modular-admin/css/app-orange.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/style-main.css') }}">
+        <title>Library</title>
+    </head>
+    <body>
+           <div class="main-wrapper">
+                <div class="app" id="app">
+                    <div class="sidebar-overlay" id="sidebar-overlay"></div>
+                    <div class="sidebar-mobile-menu-handle" id="sidebar-mobile-menu-handle"></div>
+                    <div class="mobile-menu-handle"></div>
+                    @yield('content')                              
+                </div>
+            </div>     
+        <!-- Reference block for JS -->
+        <div class="ref" id="ref">
+            <div class="color-primary"></div>
+            <div class="chart">
+                <div class="color-primary"></div>
+                <div class="color-secondary"></div>
+            </div>
+        </div>
+        {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
+        {{-- ModularAdmin js --}}
+        <script src="{{ asset('modular-admin/js/vendor.js') }}"></script>
+        <script src="{{ asset('modular-admin/js/app.js') }}"></script>
+        @yield('script')
+    </body>
+</html>
